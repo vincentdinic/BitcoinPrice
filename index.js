1636b447e89378ab8251a9ebb37d399354929b96
vincentdinic <vincentdinic@gmail.com>
2025-03-30 16:32:25 -0400

    now using coingecko's public api, looking into how to display api data onto html page

--- index.js
+++ index.js
@@ -1,18 +1,50 @@
 require('dotenv').config()
 
-console.log(process.env.API_KEY)
-
 const options = {
     method: 'GET',
-    headers: {accept: 'application/json', 'x-cg-pro-api-key': process.env.API_KEY}
+    headers: {accept: 'application/json', 'x-cg-api-key': process.env.API_KEY}
 };
 
 // Fetches information from CoinGecko's Demo API
-fetch('https://api.coingecko.com/api/v3/derivatives', options)
+let btcUSD = "";
+let desc = "";
+fetch('https://api.coingecko.com/api/v3/coins/bitcoin', options)
     .then(res => res.json())
     .then(data => {
-        data = data.filter(item => item.market === 'Binance (Futures)')
-        const btcUSD = data.filter(item => item.index_id === 'BTC')
+        console.log(data)
+        console.log(typeof(data.description.en))
+        desc = String(data.description.en);
+        console.log(desc)
+        console.log(data.market_data.current_price.usd)
+
+
+        btcUSD = String(data.market_data.current_price.usd)
         console.log(btcUSD)
-    })
+
+        console.log(data.market_data.low_24h.usd)
+        console.log(data.market_data.high_24h.usd)
+        // market_data.ath
+        // market_data.ath_date
+        // market_data.atl
+        displayBitcoinPrice(btcUSD);
+        
+
+    })  
     .catch(err => console.error(err));
+
+// TODO - This is wrong. Node.js doesn't have DOM objects
+// Look into using cron 
+function displayBitcoinPrice(btcUSD){
+    let descElement = document.getElementById("desc");
+    console.log(desc)
+    descElement.innerHTML = desc;
+    let priceElement = document.getElementById("price");
+    console.log("hey" + btcUSD)
+
+    priceElement.innerHTML = btcUSD;
+    
+}
+// Waits for the DOM to load before changing elements values
+// window.onload = function() {
+//     displayBitcoinPrice(); 
+// };
